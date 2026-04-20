6dcaddd8c1b39c0363cbbe393e9de695a10a9c01
avallete <andrew.valleteau@supabase.io>
2026-04-20 16:11:58 +0200

    supabase-shim-poc 0.0.2

--- Formula/supabase-shim-poc.rb
+++ Formula/supabase-shim-poc.rb
@@ -1,26 +1,26 @@
 class SupabaseShimPoc < Formula
   desc "Supabase CLI"
   homepage "https://supabase.com"
-  version "0.0.1"
+  version "0.0.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/avallete/supabase-cli-release-poc/releases/download/v0.0.1/supabase_0.0.1_darwin_arm64.tar.gz"
-      sha256 "5e4cad78210c1670335d92e500cf208a5e7c171221b5abe8449f8f76547e02d1"
+      url "https://github.com/avallete/supabase-cli-release-poc/releases/download/v0.0.2/supabase_0.0.2_darwin_arm64.tar.gz"
+      sha256 "2f30d6f542fe53fa11af7f01ca441c7ca44308426e2a630f8bddbacb7fbb8a20"
     else
-      url "https://github.com/avallete/supabase-cli-release-poc/releases/download/v0.0.1/supabase_0.0.1_darwin_amd64.tar.gz"
-      sha256 "ef8616d5b89f70327a942bfd9d281c78624c9aa52e79305e0e62681f3dffe14a"
+      url "https://github.com/avallete/supabase-cli-release-poc/releases/download/v0.0.2/supabase_0.0.2_darwin_amd64.tar.gz"
+      sha256 "af7728e322a32698d1d8f49852a9e823eff1225f2bacdbfe03793f591df16c5c"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/avallete/supabase-cli-release-poc/releases/download/v0.0.1/supabase_0.0.1_linux_arm64.tar.gz"
-      sha256 "dec0ed0ada94f8c32ff982455e411c7da72822de4e458ef5e9340a247345aa98"
+      url "https://github.com/avallete/supabase-cli-release-poc/releases/download/v0.0.2/supabase_0.0.2_linux_arm64.tar.gz"
+      sha256 "d79c20045874f374b6178dd0bc522c9035405769884e9a3119307041fd4aa44b"
     else
-      url "https://github.com/avallete/supabase-cli-release-poc/releases/download/v0.0.1/supabase_0.0.1_linux_amd64.tar.gz"
-      sha256 "22d97d46170f04222e73ce05288e036bef773875c78adebd02c6d2d40b8001b3"
+      url "https://github.com/avallete/supabase-cli-release-poc/releases/download/v0.0.2/supabase_0.0.2_linux_amd64.tar.gz"
+      sha256 "f5534d56b0a196b9057aa3a1881e0497624263bbc4484e1fd990518fb0a81930"
     end
   end
 
